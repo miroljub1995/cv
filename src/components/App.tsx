303a259ff5f859872fd5a03b993a1b6dd244ff57
Miroljub <miroljub1995@gmail.com>
2020-07-16 12:50:44 +0200

    added education

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,11 +1,18 @@
 import React from 'react'
 import './App.css'
 import PersonalInfo from './PersonalInfo'
+import Education from './Education'
 
 function App() {
   return (
     <div className="app">
-      <PersonalInfo />
+      <div className='column'>
+        <PersonalInfo />
+      </div>
+      <div className='column'>
+        <h1>Miroljub Tomic</h1>
+        <Education />
+      </div>
     </div>
   )
 }
---
+++ src/components/Education.tsx
@@ -0,0 +1,39 @@
+import React from 'react'
+import { studies } from '../data/education.json'
+import './Education.css'
+
+type Studies = {
+  university: string,
+  from: string,
+  to: string,
+  school: string,
+  studies: string,
+  department: string,
+  module?: string,
+  GPAByNow?: string,
+  GPA?: string
+}
+
+const Items = () => (
+  <div className='column'>
+    {studies.map((studies: Studies, key) => (
+      <div key={key} className='studies column'>
+        <div>
+          <b>{studies.university}</b>
+          <p>{`${studies.from} - ${studies.to}`}</p>
+        </div>
+        <div>
+          {`${studies.school}`}
+        </div>
+        <div>{`${studies.studies}, Department of ${studies.department}`}</div>
+      </div>
+    ))}
+  </div>
+)
+
+export default () => (
+  <>
+    <h3>Eduation</h3>
+    <Items />
+  </>
+)
