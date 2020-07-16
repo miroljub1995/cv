ce80e973c11dda9b63094974e154a06a217c815a
Miroljub <miroljub1995@gmail.com>
2020-07-16 08:24:08 +0200

    rename variable open links in new tab

--- src/components/PersonalInfo.tsx
+++ src/components/PersonalInfo.tsx
@@ -1,29 +1,29 @@
 import React from 'react'
 import './PersonalInfo.css'
-import PersonalInfoData from '../data/personalInfo.json'
+import * as data from '../data/personalInfo.json'
 import profileImage from '../data/profileImage.jpg'
 
 const Contact = () => (
   <div className='contact'>
     <div>
       <p>Mobile:</p>
-      <a href={`tel:${PersonalInfoData.contact.mobile}`}>{PersonalInfoData.contact.mobile}</a>
+      <a href={`tel:${data.contact.mobile}`}>{data.contact.mobile}</a>
     </div>
     <div>
       <p>Email:</p>
-      <a href={`mailto:${PersonalInfoData.contact.email}`}>{PersonalInfoData.contact.email}</a>
+      <a href={`mailto:${data.contact.email}`}>{data.contact.email}</a>
     </div>
     <div>
       <p>Location:</p>
-      <p>{`${PersonalInfoData.contact.city}, ${PersonalInfoData.contact.country}`}</p>
+      <p>{`${data.contact.city}, ${data.contact.country}`}</p>
     </div>
     <div>
       <p>LinkedIn:</p>
-      <a href={PersonalInfoData.contact.linkedIn}>Visit profile</a>
+      <a href={data.contact.linkedIn} target="_blank">Visit profile</a>
     </div>
     <div>
       <p>GitHub:</p>
-      <a href={PersonalInfoData.contact.github}>Visit profile</a>
+      <a href={data.contact.github} target="_blank">Visit profile</a>
     </div>
   </div>
 )
@@ -35,13 +35,13 @@ export default function PersonalInfo() {
       <h3>Personal info</h3>
       <div className='birth-date'>
         <p>Birth date</p>
-        <p>{PersonalInfoData.birthDate}</p>
+        <p>{data.birthDate}</p>
       </div>
-      <p className='align-justify'>{PersonalInfoData.aboutMe}</p>
+      <p className='align-justify'>{data.aboutMe}</p>
       <h3>Contact</h3>
       <Contact />
       <h3>Soft skills</h3>
-      <p className='align-justify'>{PersonalInfoData.softSkills}</p>
+      <p className='align-justify'>{data.softSkills}</p>
     </div>
   )
 }
