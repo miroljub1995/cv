9635a2ab231d20e3279aeda7e658880c2ed544a3
miroljub1995 <46632388+miroljub1995@users.noreply.github.com>
2020-07-18 18:34:44 +0000

    Deploying to gh-pages from  @ 5099d9424560673725bb9138160f2d74eb035244 🚀

---
+++ precache-manifest.bf589e4de74398c749c894a1d37f7d3e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3379cddc48c0a6105b8bc2eec6e994f4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5b7ff2a02e957b924a4b",
+    "url": "/static/css/main.63890070.chunk.css"
+  },
+  {
+    "revision": "06a5feb82a0ae99cdcb9",
+    "url": "/static/js/2.80668394.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.80668394.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5b7ff2a02e957b924a4b",
+    "url": "/static/js/main.2cb8037c.chunk.js"
+  },
+  {
+    "revision": "b63a4b8ca5166987a4a7",
+    "url": "/static/js/runtime-main.2ca0876f.js"
+  },
+  {
+    "revision": "05cc83dee5cc886246b74c40f9d5b975",
+    "url": "/static/media/profileImage.05cc83de.jpg"
+  }
+]);
